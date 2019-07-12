83bc14b330105c2132b4a5acf67a668810372506
Benjamin <benjaminbillard34@gmail.com>
2019-07-12 14:38:19 +0200

    generator

--- reporter/generator.py
+++ reporter/generator.py
@@ -1,4 +1,26 @@
-def greet():
-    """ A realy minimal function
-    """
-    print("Hello World!")
+from reporter import header
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Generator:
+    def __init__(self, json_path, xls_path, md_path):
+        self.json_path = json_path
+        self.xls_path = xls_path
+        self.md_path = md_path
+        
+        with open(json_path, "r") as in_file:
+            self.reloaded = json.load(in_file)
+    
+        self.series = pd.read_excel(xls_path).set_index("année").transpose().dropna(how="all", axis=1)
+
+    def create_graph(self):
+        self.series.plot(y=['PIB'])
+        plt.savefig('graph.png')
+
+    
+    def create_markdown(self):
+        var_md = header.create_header(self.reloaded) + "\n\n![a graph](graph.png)\n"
+        
+        with open(self.md_path, 'w', encoding='utf-8') as out_file:
+            out_file.write(var_md)
--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -1,2 +1,30 @@
+from reporter import header
+
 def test_reporter():
     assert True
+
+
+def test_create_header():
+
+    author1 = {
+        'firstname': 'Miles',
+        'lastname': 'Davis',
+        'DOB': 1926
+    }
+
+    author2 = {
+        'firstname': 'John',
+        'lastname': 'Coltrane',
+        'DOB': 1933
+    }
+
+    author3 = {
+        'firstname': 'Alfred',
+        'lastname': 'McCoy Tyner',
+        'DOB': 1938
+    }
+    authors = [author1,author2,author3]
+    
+    header_txt = header.create_header(authors)
+    assert "Alfred" in header_txt
+    assert "McCoy" in header_txt
